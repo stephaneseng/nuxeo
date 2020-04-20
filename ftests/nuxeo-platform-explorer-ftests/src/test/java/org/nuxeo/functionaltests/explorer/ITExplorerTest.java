401f698d2735898fb848cbe1144093c2605e5a60
Anahide Tchertchian <at@nuxeo.com>
2020-04-20 16:52:22 +0200

    NXP-28839: add functional tests

--- ftests/nuxeo-platform-explorer-ftests/src/test/java/org/nuxeo/functionaltests/explorer/ITExplorerTest.java
+++ ftests/nuxeo-platform-explorer-ftests/src/test/java/org/nuxeo/functionaltests/explorer/ITExplorerTest.java
@@ -17,11 +17,20 @@
  */
 package org.nuxeo.functionaltests.explorer;
 
-import static org.nuxeo.functionaltests.Constants.ADMINISTRATOR;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.nuxeo.functionaltests.AbstractTest;
+import org.nuxeo.functionaltests.RestHelper;
+import org.nuxeo.functionaltests.explorer.pages.ArtifactHomePage;
+import org.nuxeo.functionaltests.explorer.pages.ArtifactPage;
+import org.nuxeo.functionaltests.explorer.pages.ExplorerHomePage;
 import org.nuxeo.functionaltests.pages.DocumentBasePage.UserNotConnectedException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * Test explorer main webengine pages.
@@ -30,14 +39,183 @@ import org.nuxeo.functionaltests.pages.DocumentBasePage.UserNotConnectedExceptio
  */
 public class ITExplorerTest extends AbstractTest {
 
+    @Before
+    public void before() {
+        RestHelper.createUser(TEST_USERNAME, TEST_PASSWORD, null, null, null, null, "members");
+    }
+
+    @After
+    public void after() {
+        RestHelper.cleanup();
+    }
+
+    protected void doLogin() {
+        getLoginPage().login(TEST_USERNAME, TEST_PASSWORD);
+    }
+
+    protected void doLogout() {
+        // logout avoiding JS error check
+        driver.get(NUXEO_URL + "/logout");
+    }
+
+    protected ExplorerHomePage goHome() {
+        open(ExplorerHomePage.URL);
+        return asPage(ExplorerHomePage.class);
+    }
+
     /**
      * Simple login, logout test, checking the home page is displayed without errors after login.
      */
     @Test
     public void testLoginLogout() throws UserNotConnectedException {
-        getLoginPage().login(ADMINISTRATOR, ADMINISTRATOR);
-        open("/site/distribution");
-        // logout avoiding JS error check
-        driver.get(NUXEO_URL + "/logout");
+        doLogin();
+        goHome();
+        doLogout();
     }
+
+    @Test
+    public void testHomePage() throws UserNotConnectedException {
+        doLogin();
+        ExplorerHomePage home = goHome();
+        assertEquals("Nuxeo Platform Explorer", home.getTitle());
+        assertEquals("Running Platform".toUpperCase(), home.currentPlatform.getText());
+
+        ArtifactHomePage ahome = home.navigateTo(home.currentExtensionPoints);
+        assertEquals("All extension points", ahome.getTitle());
+        ahome = goHome().navigateTo(home.currentContributions);
+        assertEquals("All contributions", ahome.getTitle());
+        ahome = goHome().navigateTo(home.currentExtensionPoints);
+        assertEquals("All extension points", ahome.getTitle());
+        ahome = goHome().navigateTo(home.currentOperations);
+        assertEquals("All operations", ahome.getTitle());
+        ahome = goHome().navigateTo(home.currentServices);
+        assertEquals("All services", ahome.getTitle());
+
+        doLogout();
+    }
+
+    @Test
+    public void testExtensionPoints() throws UserNotConnectedException {
+        doLogin();
+        ExplorerHomePage home = goHome();
+        ArtifactHomePage ahome = home.navigateTo(home.currentExtensionPoints);
+        assertTrue(ahome.isSelected(ahome.extensionPoints));
+        WebElement elt = ahome.getFirstListingElement();
+        WebElement link = elt.findElement(By.xpath(".//a"));
+        assertEquals("actions", link.getText());
+        assertEquals("ActionService", elt.findElement(By.xpath(".//span[@title='Component Label']")).getText());
+        assertEquals("org.nuxeo.ecm.platform.actions.ActionService",
+                elt.findElement(By.xpath(".//span[@title='Component ID']")).getText());
+        link.click();
+        ArtifactPage apage = asPage(ArtifactPage.class);
+        assertTrue(apage.isSelected(apage.extensionPoints));
+        assertEquals("Extension point org.nuxeo.ecm.platform.actions.ActionService--actions", apage.getTitle());
+        assertEquals("Extension point actions", apage.header.getText());
+        assertEquals("In component org.nuxeo.ecm.platform.actions.ActionService", apage.description.getText());
+        doLogout();
+    }
+
+    @Test
+    public void testContributions() throws UserNotConnectedException {
+        doLogin();
+        ExplorerHomePage home = goHome();
+        ArtifactHomePage ahome = home.navigateTo(home.currentExtensionPoints);
+        ahome = ahome.navigateTo(ahome.contributions);
+        assertTrue(ahome.isSelected(ahome.contributions));
+        WebElement elt = ahome.getFirstListingElement();
+        WebElement link = elt.findElement(By.xpath(".//a"));
+        assertEquals("cluster-config--configuration", link.getText());
+        assertEquals("configuration", elt.findElement(By.xpath(".//span[@title='Target Extension Point']")).getText());
+        assertEquals("org.nuxeo.runtime.cluster.ClusterService",
+                elt.findElement(By.xpath(".//span[@title='Target Component Name']")).getText());
+        link.click();
+        ArtifactPage apage = asPage(ArtifactPage.class);
+        assertTrue(apage.isSelected(apage.contributions));
+        assertEquals("Contribution cluster-config--configuration", apage.getTitle());
+        assertEquals("Contribution cluster-config--configuration", apage.header.getText());
+        doLogout();
+    }
+
+    @Test
+    public void testServices() throws UserNotConnectedException {
+        doLogin();
+        ExplorerHomePage home = goHome();
+        ArtifactHomePage ahome = home.navigateTo(home.currentExtensionPoints);
+        ahome = ahome.navigateTo(ahome.services);
+        assertTrue(ahome.isSelected(ahome.services));
+        WebElement elt = ahome.getFirstListingElement();
+        WebElement link = elt.findElement(By.xpath(".//a"));
+        assertEquals("ActionManager", link.getText());
+        assertEquals("org.nuxeo.ecm.platform.actions.ejb.ActionManager",
+                elt.findElement(By.xpath(".//span[@title='Service Name']")).getText());
+        link.click();
+        ArtifactPage apage = asPage(ArtifactPage.class);
+        assertTrue(apage.isSelected(apage.services));
+        assertEquals("Service org.nuxeo.ecm.platform.actions.ejb.ActionManager", apage.getTitle());
+        assertEquals("Service org.nuxeo.ecm.platform.actions.ejb.ActionManager", apage.header.getText());
+        assertEquals("In component org.nuxeo.ecm.platform.actions.ActionService", apage.description.getText());
+        doLogout();
+    }
+
+    @Test
+    public void testOperations() throws UserNotConnectedException {
+        doLogin();
+        ExplorerHomePage home = goHome();
+        ArtifactHomePage ahome = home.navigateTo(home.currentExtensionPoints);
+        ahome = ahome.navigateTo(ahome.operations);
+        assertTrue(ahome.isSelected(ahome.operations));
+        WebElement elt = ahome.getFirstListingElement();
+        WebElement link = elt.findElement(By.xpath(".//a"));
+        assertEquals("acceptComment", link.getText());
+        assertEquals("CHAIN", elt.findElement(By.xpath(".//span[@title='Category']")).getText());
+        assertEquals("acceptComment", elt.findElement(By.xpath(".//span[@title='Operation ID']")).getText());
+        link.click();
+        ArtifactPage apage = asPage(ArtifactPage.class);
+        assertTrue(apage.isSelected(apage.operations));
+        assertEquals("Operation acceptComment", apage.getTitle());
+        assertEquals("Operation acceptComment", apage.header.getText());
+        assertEquals("acceptComment", apage.description.getText());
+        doLogout();
+    }
+
+    @Test
+    public void testComponents() throws UserNotConnectedException {
+        doLogin();
+        ExplorerHomePage home = goHome();
+        ArtifactHomePage ahome = home.navigateTo(home.currentExtensionPoints);
+        ahome = ahome.navigateTo(ahome.components);
+        assertTrue(ahome.isSelected(ahome.components));
+        WebElement elt = ahome.getFirstListingElement();
+        WebElement link = elt.findElement(By.xpath(".//a"));
+        assertEquals("actions.ActionService", link.getText());
+        assertEquals("JAVA", elt.findElement(By.xpath(".//span[@title='Component Type']")).getText());
+        assertEquals("org.nuxeo.ecm.platform.actions.ActionService",
+                elt.findElement(By.xpath(".//span[@title='Component ID']")).getText());
+        link.click();
+        ArtifactPage apage = asPage(ArtifactPage.class);
+        assertTrue(apage.isSelected(apage.components));
+        assertEquals("Component org.nuxeo.ecm.platform.actions.ActionService", apage.getTitle());
+        assertEquals("Component org.nuxeo.ecm.platform.actions.ActionService", apage.header.getText());
+        assertEquals("In bundle org.nuxeo.ecm.actions", apage.description.getText());
+        doLogout();
+    }
+
+    @Test
+    public void testBundles() throws UserNotConnectedException {
+        doLogin();
+        ExplorerHomePage home = goHome();
+        ArtifactHomePage ahome = home.navigateTo(home.currentExtensionPoints);
+        ahome = ahome.navigateTo(ahome.bundles);
+        assertTrue(ahome.isSelected(ahome.bundles));
+        WebElement elt = ahome.getFirstListingElement();
+        WebElement link = elt.findElement(By.xpath(".//a"));
+        assertEquals("org.nuxeo.admin.center", link.getText());
+        link.click();
+        ArtifactPage apage = asPage(ArtifactPage.class);
+        assertTrue(apage.isSelected(apage.bundles));
+        assertEquals("Bundle org.nuxeo.admin.center", apage.getTitle());
+        assertEquals("Bundle org.nuxeo.admin.center", apage.header.getText());
+        doLogout();
+    }
+
 }
